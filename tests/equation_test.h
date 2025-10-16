101e1d82a0075f4233066dfac152e2e545263ae0
Nikita Doskal <120043683+DoNik226@users.noreply.github.com>
2025-10-16 22:10:24 +0300

    Update equation_test.h

--- tests/equation_test.h
+++ tests/equation_test.h
@@ -13,20 +13,20 @@ extern "C" {
 }
 
 TEST(equationTest, size0) {
-    _Complex root1, root2;
+    double root1, root2;
     ASSERT_EQ(equation(1, -1, 5, &root1, &root2), 0);
     ASSERT_DOUBLE_EQ(root1, 0.5);
     ASSERT_DOUBLE_EQ(roundf(root2 * 100) / 100, roundf(2.18 * 100) / 100);
 }
 
 TEST(equationTest, size1) {
-    _Complex root1, root2;
+    double root1, root2;
     ASSERT_EQ(equation(1, -4, 4, &root1, &root2), 1);
     ASSERT_DOUBLE_EQ(root1, 2);
 }
 
 TEST(equationTest, size2) {
-    _Complex root1, root2;
+    double root1, root2;
     ASSERT_EQ(equation(1, -4, 3, &root1, &root2), 2);
     ASSERT_DOUBLE_EQ(root1, 3);
     ASSERT_DOUBLE_EQ(root2, 1);
