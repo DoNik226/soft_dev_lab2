015dbdcd31c95e01c247525c28b07b93ea898b38
Nikita Doskal <120043683+DoNik226@users.noreply.github.com>
2025-10-16 22:01:09 +0300

    Update equation.c

--- app/equation.c
+++ app/equation.c
@@ -1,22 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <math.h>
-#include "equation.h"
+#include <complex.h>
 
-int equation(double a, double b, double c, double *root1, double *root2) {
-    double d = b * b - 4 * a * c;
+int equation(double a, double b, double c, double _Complex *root1, double _Complex *root2) {
+    double d = b * b - 4.0 * a * c;
+    double den = 2.0 * a;
 
     if (d > 0) {
-        *root1 = (-b + sqrt(d)) / (2 * a);
-        *root2 = (-b - sqrt(d)) / (2 * a);
+        double r1 = (-b - sqrt(d)) / den;
+        double r2 = (-b + sqrt(d)) / den;
+        *root1 = r1 + 0.0*I;
+        *root2 = r2 + 0.0*I;
         return 2;
     } else if (d == 0) {
-        *root1 = -b / (2 * a);
-        *root2 = 0;
+        double r = -b / den;
+        *root1 = r + 0.0*I;
+        *root2 = r + 0.0*I;
         return 1;
     } else {
-        *root1 = -b / (2 * a);     // Действительная часть
-        *root2 = sqrt(-d) / (2 * a); // Мнимая часть
+        *root1 = (-b + I * sqrt(-d)) / den;
+        *root2 = (-b - I * sqrt(-d)) / den;
         return 0;
     }
 }
