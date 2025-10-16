03b2464584c90939148329f449bd97d2fe639a28
Nikita Doskal <120043683+DoNik226@users.noreply.github.com>
2025-10-16 22:02:02 +0300

    Update equation.h

--- app/equation.h
+++ app/equation.h
@@ -1,6 +1,6 @@
 #ifndef EQUATION_H
 #define EQUATION_H
 
-int equation(double a, double b, double c, double *root1, double *root2);
+int equation(double a, double b, double c, _Complex *root1, _Complex *root2);
 
 #endif // EQUATION_H
