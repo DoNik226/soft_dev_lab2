465848bcdfe964001e68f3c371ec9fa913b18773
Nikita Doskal <120043683+DoNik226@users.noreply.github.com>
2025-10-16 22:13:11 +0300

    Update equation.c

--- app/equation.c
+++ app/equation.c
@@ -2,24 +2,28 @@
 #include <math.h>
 #include <complex.h>
 
-int equation(double a, double b, double c, double _Complex *root1, double _Complex *root2) {
-    double d = b * b - 4.0 * a * c;
-    double den = 2.0 * a;
+int equation(double a, double b, double c, double double *root1, double double *root2) {
+    double d = b * b - 4 * a * c;
 
     if (d > 0) {
-        double r1 = (-b - sqrt(d)) / den;
-        double r2 = (-b + sqrt(d)) / den;
-        *root1 = r1 + 0.0*I;
-        *root2 = r2 + 0.0*I;
+        *root1 = (-b + sqrt(d)) / (2 * a);
+        *root2 = (-b - sqrt(d)) / (2 * a);
+        printf("Два действительных корня:\n");
+        printf("x1 = %.2lf\n", *root1);
+        printf("x2 = %.2lf\n", *root2);
         return 2;
     } else if (d == 0) {
-        double r = -b / den;
-        *root1 = r + 0.0*I;
-        *root2 = r + 0.0*I;
+        *root1 = -b / (2 * a);
+        *root2 = 0;
+        printf("Один действительный корень:\n");
+        printf("x = %.2lf\n", *root1);
         return 1;
     } else {
-        *root1 = (-b + I * sqrt(-d)) / den;
-        *root2 = (-b - I * sqrt(-d)) / den;
+        *root1 = -b / (2 * a);     // Действительная часть
+        *root2 = sqrt(-d) / (2 * a); // Мнимая часть
+        printf("Комплексные корни:\n");
+        printf("x1 = %.2lf + %.2lfi\n", *root1, *root2);
+        printf("x2 = %.2lf - %.2lfi\n", *root1, *root2);
         return 0;
     }
 }
