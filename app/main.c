25af2b5e5c4d04d0bb28fd0b7c7718f6c358fc36
Nikita Doskal <120043683+DoNik226@users.noreply.github.com>
2025-10-16 21:50:52 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -8,25 +8,10 @@ int main()
     double root1, root2;
     int rootsCount;
     printf("Hello World!\n");
-    rootsCount = equation(1, -1, 5, &root1, &root2);
+    equation(1, -1, 5, &root1, &root2);
 
-    switch(rootsCount) {
-    case 2:
-        printf("Два действительных корня:\n");
-        printf("x1 = %.2lf\n", root1);
-        printf("x2 = %.2lf\n", root2);
-        break;
-
-    case 1:
-        printf("Один действительный корень:\n");
-        printf("x = %.2lf\n", root1);
-        break;
-
-    case 0:
-        printf("Комплексные корни:\n");
-        printf("x1 = %.2lf + %.2lfi\n", root1, root2);
-        printf("x2 = %.2lf - %.2lfi\n", root1, root2);
-        break;
-    }
+    printf("x1 = %.2lf\n", root1);
+    printf("x2 = %.2lf\n", root2);
+    
     return myfunc(2);
 }
